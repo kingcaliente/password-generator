43f055bb962729fe14580cd7f7662dcb5647f9c0
Snack Man <lpardo1989@gmail.com>
2023-05-05 13:36:09 -0700

    modified script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//created an object with the 
+//created an object with the output from our 4 functions
 
 var random = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
+  lower: getRandomLower(),
+  upper: getRandomUpper(),
+  number: getRandomNumber(),
+  symbol: getRandomSymbol(),
 }
 
 
@@ -15,10 +15,45 @@ var generateElement = document.getElementById('result');
 
 
 
+//generating a function to prompt the user for the password requisites 
+
 function generate(){
-  prompt ('Welcome to The Password Generator, How many characters in your Password?');
-  return random
+numberOfCharacters = prompt("How many characters do you want in your password? Note: It must be between 8 and 128 characters for password security");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    return "Please choose a valid number of characters.";
+  } else if (isNaN(numberOfCharacters)) {
+    numberOfCharacters = prompt("Please enter a valid number.");
+  }
+  else {
+    alert("Your password will be " + numberOfCharacters + " characters long.");
     
+  }
+
+grammar = confirm("Should we include a mix of uppercase and lowercase? *** Press OK for Yes or Cancel for No***");
+if (grammar) {
+  var turnToLowercase = alert("The password will include a mix of lowercase and uppercase letters");
+}
+else {
+  alert("Your password will NOT have a mix of lowercase and uppercase letters.");
+}
+
+numerals = confirm("Should we include numbers in your password? *** Press OK for Yes or Cancel for No***");
+  if (numerals) {
+    alert ("The Password will include numbers");
+  }
+  else {
+    alert("No numbers will be included in the password");
+  }
+
+hieroglyphics = confirm("Should we include special characters in your password?");
+  if(hieroglyphics) {
+    alert ("We will include special characters in the new password");
+  } 
+  else {
+    alert ("The password will not include any special characters");
+  }
+
+
 }
 
 
